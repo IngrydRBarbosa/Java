9f337bdb1d2022fb11b96aae84ffd50207b5b304
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-22 21:03:18 -0300

    Ajustes

---
+++ Aula2/CalculadoraMedia.java
@@ -0,0 +1,13 @@
+package Aula2;
+
+public class CalculadoraMedia {
+    public static void main(String[] args){
+        double Nota1 = 7;
+        double Nota2 = 5;
+        double Nota3 = 10;
+        double Nota4 = 8;
+        double Media = (Nota1+Nota2+Nota3+Nota4)/2;
+
+        System.out.println("A media e: " + Media);
+    }
+}
---
+++ Aula2/ExercicioVariaveis.java
@@ -0,0 +1,15 @@
+package Aula2;
+
+public class ExercicioVariaveis {
+    public static void main(String[] args){
+        String Nome = "Ingryd";
+        int Idade = 20;
+        double Peso = 80;
+        double Altura = 1.60;
+
+        System.out.println("A funcionária " + Nome + " tem:");
+        System.out.println("Idade: " + Idade + " anos");
+        System.out.println("Altura: " + Altura + "m");
+        System.out.println("Peso: " + Peso + "kg");
+    } 
+}
