8a4f041c7daba2cd0be24f1b587912a4b4a4a662
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-22 21:17:36 -0300

    Ajuste

--- Aula2/ExercicioVariaveis.java
+++ Aula2/ExercicioVariaveis.java
@@ -4,11 +4,12 @@ public class ExercicioVariaveis {
     public static void main(String[] args){
         String nome = "Ingryd";
         int idade = 20;
-        double peso = 80, altura = 1.60;
+        float peso = 80f;
+        double Altura = 1.60;
 
         System.out.println("A funcionária " + nome + " tem:");
         System.out.println("Idade: " + idade + " anos");
-        System.out.println("Altura: " + altura + "m");
+        System.out.println("Altura: " + Altura + "m");
         System.out.println("Peso: " + peso + "kg");
     } 
 }
