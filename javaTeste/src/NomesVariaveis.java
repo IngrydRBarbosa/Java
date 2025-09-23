4c2297c62a1d3c0806ad3bb8b8f50df2aff1ae33
IngrydRBarbosa <ingrydbarbosa2008@gmail.com>
2025-09-23 16:35:39 -0300

    diferenças entre variáveis

---
+++ javaTeste/src/NomesVariaveis.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class NomesVariaveis {
+    public static void main(String[] args) {
+        /*
+        Números inteiros:
+         byte = -128 a 127
+         short -> int -> long (ex: 1000L)
+         Números decimais:
+         float -> precisão simples (ex: 10.5f)
+         double -> precisão dupla
+
+         String -> palavra ou frase (usa aspas duplas)
+         char -> 1 caracter (usa aspas simples)
+        */
+    }
+}
