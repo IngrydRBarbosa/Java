3698c80f6741cc4bd2ac73b23c60b19c25e6781d
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-23 20:23:17 -0300

    if e else

---
+++ Exercicios/ifELse.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+public class ifELse {
+    public static void main(String[] args) {
+        byte n1 = 5, n2 = 10;
+
+        if (n1>n2){
+            System.out.println("O numero 1 é maior do que o numero 2.");
+        }else if(n1<n2){
+            System.out.println("O numero 1 é menor do que o numero 2");
+        }else {
+            System.out.println("Os numeros sao iguais");
+        }
+    }
+}
