4f4eef8d856791007b929162592f04c58b49fd1c
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-23 18:43:54 -0300

    Exercícios aula 2

---
+++ ex2ConversorMoeda.java
@@ -0,0 +1,9 @@
+public class ex2ConversorMoeda {
+    public static void main(String[] args) {
+        double valorEmDolar = 32;
+        double taxaDeCambio = 5.25;
+        double valorEmReal = valorEmDolar * taxaDeCambio;
+
+        System.out.println("O valor em reais (R$) e: " + valorEmReal);
+    }
+}
---
+++ ex3DivisaoComResto.java
@@ -0,0 +1,8 @@
+public class ex3DivisaoComResto {
+    public static void main(String[] args){
+        int totalDeItens = 17, capacidadeDaCaixa = 5;
+        int caixasPreenchidas = totalDeItens/capacidadeDaCaixa, sobras = totalDeItens%capacidadeDaCaixa;
+
+        System.out.println("Serao preenchidas " + caixasPreenchidas + " caixas\nSobrarao " + sobras + " itens.");
+    }
+}
