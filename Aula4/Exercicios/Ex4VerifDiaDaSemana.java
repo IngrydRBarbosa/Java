04909e0a01d2ba894b7df662e98262aa1dc6bfa1
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-24 19:32:59 -0300

    teste de código menor

--- Aula4/Exercicios/Ex4VerifDiaDaSemana.java
+++ Aula4/Exercicios/Ex4VerifDiaDaSemana.java
@@ -2,31 +2,17 @@ package Aula4.Exercicios;
 
 public class Ex4VerifDiaDaSemana {
     public static void main(String[] args) {
-        byte diaDaSemana = 7;
+        byte diaDaSemana = 4;
 
         switch (diaDaSemana) {
-            case 1:
+            case 1, 7:
                 System.out.println("É final de semana! :D");
                 break;
-            case 2:
+            case 2, 3, 4, 5, 6:
                 System.out.println("Estamos em dia útil :(");
-                break;
-            case 3:
-                System.out.println("Estamos em dia útil :(");
-                break;
-            case 4:
-                System.out.println("Estamos em dia útil :(");
-                break;
-            case 5:
-                System.out.println("Estamos em dia útil :(");
-                break;
-            case 6:
-                System.out.println("Estamos em dia útil :(");
-                break;
-            case 7:
-                System.out.println("É final de semana! :D");
                 break;
             default:
+                System.out.println("Número inválido");
                 break;
         }
     }
