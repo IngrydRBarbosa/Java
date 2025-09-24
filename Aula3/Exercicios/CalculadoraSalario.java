44ea44775e42274c74107c7fba74c18250bec165
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-24 18:39:49 -0300

    ideia com código menor

--- Aula3/Exercicios/CalculadoraSalario.java
+++ Aula3/Exercicios/CalculadoraSalario.java
@@ -6,13 +6,13 @@ public class CalculadoraSalario {
         float salario = 1690f;
 
         if(salario <= 1751.81){
-            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario - (salario*8/100)));
+            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario * 0.92)); // mesma coisa que salario - (salario * 8/100) //
         }else if(salario <= 2919.72){
-            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario - (salario*9/100)));
+            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario * 0.91)); // mesma coisa que salario - (salario * 9/100) //
         }else if(salario <= 5839.45){
-            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario - (salario*10/100)));
+            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario * 0.9)); // mesma coisa que salario - (salario * 10/100) //
         }else if(salario > 5839.456){
-            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario - (salario*11/100)));
+            System.out.println(nome + ", seu salario com desconto do INSS e R$" + (salario * 0.89)); // mesma coisa que salario - (salario * 11/100) //
         }
     }
 }
