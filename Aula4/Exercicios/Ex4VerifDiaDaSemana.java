ccf64c6ba2716fafa0900b003ab30f18e0224008
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-24 19:28:58 -0300

    Adicionei Ex4 Aula4

---
+++ Aula4/Exercicios/Ex4VerifDiaDaSemana.java
@@ -0,0 +1,33 @@
+package Aula4.Exercicios;
+
+public class Ex4VerifDiaDaSemana {
+    public static void main(String[] args) {
+        byte diaDaSemana = 7;
+
+        switch (diaDaSemana) {
+            case 1:
+                System.out.println("É final de semana! :D");
+                break;
+            case 2:
+                System.out.println("Estamos em dia útil :(");
+                break;
+            case 3:
+                System.out.println("Estamos em dia útil :(");
+                break;
+            case 4:
+                System.out.println("Estamos em dia útil :(");
+                break;
+            case 5:
+                System.out.println("Estamos em dia útil :(");
+                break;
+            case 6:
+                System.out.println("Estamos em dia útil :(");
+                break;
+            case 7:
+                System.out.println("É final de semana! :D");
+                break;
+            default:
+                break;
+        }
+    }
+}
