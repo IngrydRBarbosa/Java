75167d71f33dc6b6610315acdea96109d3b36c12
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-25 18:51:16 -0300

    teste Vetor

--- Aula4/Exercicios/Ex3MediaNotas.java
+++ Aula4/Exercicios/Ex3MediaNotas.java
@@ -1,18 +1,30 @@
 package Aula4.Exercicios;
+import java.util.Scanner;
 
 public class Ex3MediaNotas {
-    const int notas = 5;
-    public static void main(String[] args) {
-        int notasValidas = 0;
-        double mediaNotas = notasValidas/5;
 
-        for(int i=0; i<=5; i++){
-            if(i > 10){
-                continue;
-            } else if(i<0){
-                break;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 10, cont = 0;
+        int notas[] = new int[n];
+        int notasValidas = 0;
+        double mediaNotas;
+
+        for(int i=0; i<=n; i++){
+            while(cont != 5 & notasValidas != 5){
+                System.out.print("Digite sua nota: ");
+                notas[i] = scanner.nextInt();
+                if(i > 10){
+                    continue;
+                } else if(i<0){
+                    break;
+                }
+                notasValidas += notas[i];
+                cont++;
             }
         }
+        mediaNotas=notasValidas/cont;
         System.out.println("A média das notas válidas é: " + mediaNotas);
+        scanner.close();
     }
 }
