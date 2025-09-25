006f3a8436a13d2287c0596d16d827de772d2f14
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-25 19:16:12 -0300

    adicionei Ex4 e corrigi Ex1

--- Aula4/Exercicios/Ex1Tabuada.java
+++ Aula4/Exercicios/Ex1Tabuada.java
@@ -4,7 +4,7 @@ public class Ex1Tabuada {
     public static void main(String[] args) {
         int numero = 5;
         
-        for(int i = 0; i<=10; i++){
+        for(int i = 1; i<=10; i++){
             System.out.println(numero + " x " + i + " = " + numero * i);
         }
     }
