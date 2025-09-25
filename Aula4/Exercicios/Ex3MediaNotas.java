1560de54781fec85b091a2c408d9a33337131ef9
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-25 20:17:05 -0300

    correção exercícios

--- Aula4/Exercicios/Ex3MediaNotas.java
+++ Aula4/Exercicios/Ex3MediaNotas.java
@@ -5,24 +5,23 @@ public class Ex3MediaNotas {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 10, cont = 0;
-        int notas[] = new int[n];
-        int notasValidas = 0;
+        int cont = 0;
+        double notas;
+        double notasValidas = 0;
         double mediaNotas;
 
-        for(int i=0; i<=n; i++){
-            while(cont != 5 & notasValidas != 5){
-                System.out.print("Digite sua nota: ");
-                notas[i] = scanner.nextInt();
-                if(i > 10){
-                    continue;
-                } else if(i<0){
-                    break;
-                }
-                notasValidas += notas[i];
-                cont++;
+        for(int i=1; i<=5; i++){
+            System.out.printf("Digite a %dº nota: ", i);
+            notas = scanner.nextInt();
+            if(notas > 10){
+                continue;
+            } else if(notas<0){
+                break;
             }
+            notasValidas += notas;
+            cont++;
         }
+
         mediaNotas=notasValidas/cont;
         System.out.println("A média das notas válidas é: " + mediaNotas);
         scanner.close();
