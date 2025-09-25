5903dca667890beb3c0ed3a501b494089fffa5c1
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-09-24 21:14:16 -0300

    adicionei exercícios de laços de repetição

---
+++ Aula4/Exercicios/Ex1Tabuada.java
@@ -0,0 +1,11 @@
+package Aula4.Exercicios;
+
+public class Ex1Tabuada {
+    public static void main(String[] args) {
+        int numero = 5;
+        
+        for(int i = 0; i<=10; i++){
+            System.out.println(numero + " x " + i + " = " + numero * i);
+        }
+    }
+}
