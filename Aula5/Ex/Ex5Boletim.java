5a48410502d7cab2aea9aeafb7ca2d715e0fb417
Ingryd Rodrigues Barbosa <pet0160425@aluno.firjansenaisesi.com.br>
2025-10-01 18:55:57 -0300

    correção exercicio 5

--- Aula5/Ex/Ex5Boletim.java
+++ Aula5/Ex/Ex5Boletim.java
@@ -4,20 +4,21 @@ import javax.swing.JOptionPane;
 public class Ex5Boletim {
     public static void main(String[] args) {
         double[][] boletim = new double[3][4];
+        double[] medias = new double[3];
+        double soma = 0;
         
+        JOptionPane.showMessageDialog(null, "=========  BOLETIM ESCOLAR  ==========\nDigite a seguir as 4 notas bimestrais de cada aluno");
         for(int i = 0; i < boletim.length; i++){
             for(int j = 0; j < boletim[i].length; j++){
-                boletim[i][j] = Double.parseDouble(JOptionPane.showInputDialog("Digite a " + (j+1) + "ª nota do " + (i+1) + "º aluno: "));
-            }
-        }
-        String resultadoFinal = "As médias dos alunos é:\n";
-        for(int i = 0; i < boletim.length; i++) {
-            double soma = 0;
-            for(int j = 0; j < boletim[i].length; j++) {
+                boletim[i][j] = Double.parseDouble(JOptionPane.showInputDialog("Digite a " + (j+1) + "ª nota do " + "aluno " + (i+1)));
                 soma += boletim[i][j];
             }
-            double media = soma/boletim[i].length;
-            resultadoFinal += "Aluno " + (i+1) + ": " + String.format("%.2f", media) + "\n";
+            medias[i] = soma/boletim[i].length;
+            soma = 0;
+        }
+        String resultadoFinal = "As médias dos alunos são:\n";
+        for(int i = 0; i < boletim.length; i++) {
+            resultadoFinal += "Aluno " + (i+1) + ": " + String.format("%.2f", medias[i]) + "\n";
         }
         JOptionPane.showMessageDialog(null, resultadoFinal);
     }
